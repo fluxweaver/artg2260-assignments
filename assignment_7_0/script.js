76c51f60b54b2d9588c00d8516852e1003290154
fluxweaver <zavyalov.k@husky.neu.edu>
2018-03-15 16:33:01 -0400

    text

--- assignment_7_0/script.js
+++ assignment_7_0/script.js
@@ -18,6 +18,7 @@ function setup() {
     fill(random(100, 150),random(100, 150),random(100, 150), i*3);
     rect(w*i, maximum - height/2, w, height);
   }
+  	text("trend of anomalies in global temperature", 0, 190);
     fill(200, 200, 200);
    	rect(0, 200, 1500, 2);
 }
